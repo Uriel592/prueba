2fefe656e5871511352315122c6535d1e3bae8d8
uriel5 <2325285@cesba-queretaro.edu.mx>
2025-05-30 20:29:42 -0600

    Delete paginaweb directory

--- paginaweb/static/js/main.js
+++
@@ -1,9 +0,0 @@
-const btnDelete = document.querySelectorAll('.btn-danger'); 
-
-btnDelete.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    if (!confirm('Are you sure you want to delete this contact?')) {
-      e.preventDefault(); 
-    }
-  });
-});
